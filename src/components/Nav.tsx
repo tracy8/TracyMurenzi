ec21f412b1c435d6e5ff3e6a016b3cf7807072f0
Tracy Murenzi <tracymurenzi@gmail.com>
2022-02-17 16:40:49 +0200

    init

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -45,7 +45,7 @@ const Nav = () => {
               onClick={() => setShowMenu(!showMenu)}
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
             >
-              {/* Menu open: "hidden", Menu closed: "block" */}
+      
               <svg
                 className="block h-6 w-6"
                 stroke="currentColor"
@@ -59,7 +59,7 @@ const Nav = () => {
                   d="M4 6h16M4 12h16M4 18h16"
                 />
               </svg>
-              {/* Menu open: "block", Menu closed: "hidden" */}
+            
               <svg
                 className="hidden h-6 w-6"
                 stroke="currentColor"
@@ -78,7 +78,7 @@ const Nav = () => {
         </div>
       </div>
 
-      {/* Menu open: "block", Menu closed: "hidden" */}
+ 
       <div className={`md:hidden ${showMenu ? "block" : "hidden"}`}>
         <div className="px-2 pt-2 pb-3 sm:px-3">
           {links.map((link, i) => (
@@ -111,26 +111,7 @@ const Nav = () => {
               </div>
             </div>
           </div>
-          <div className="mt-3 px-2 space-y-1">
-            <a
-              href="#"
-              className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-            >
-              Your Profile
-            </a>
-            <a
-              href="#"
-              className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-            >
-              Settings
-            </a>
-            <a
-              href="#"
-              className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-            >
-              Sign out
-            </a>
-          </div>
+       
         </div>
       </div>
     </nav>
